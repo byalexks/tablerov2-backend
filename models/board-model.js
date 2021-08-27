d3c85f0861d9b5307023decede47c46d53a300a4
alexis <alexisrk310@gmail.com>
2021-08-27 17:12:03 -0500

    hecho

--- models/board-model.js
+++ models/board-model.js
@@ -1,29 +1,29 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const BoardSchema = Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  note: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    default: new Date.now(),
-  },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "Usuario",
-    required: true,
-  },
+	title: {
+		type: String,
+		required: true,
+	},
+	note: {
+		type: String,
+		required: true,
+	},
+	date: {
+		type: Date,
+		default: new Date(),
+	},
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: 'Usuario',
+		required: true,
+	},
 });
 
-BoardSchema.method("toJSON", function () {
-  const { __v, _id, ...object } = this.toObject();
-  object.id = _id;
-  return object;
+BoardSchema.method('toJSON', function () {
+	const { __v, _id, ...object } = this.toObject();
+	object.id = _id;
+	return object;
 });
 
-module.exports = model('Tablero', BoardSchema )
+module.exports = model('Tablero', BoardSchema);
